d568c6131e910591f1e93430c2ac7cc1d35e0679
ksato-dev <k-sato@smartrobotics.jp>
2019-11-16 14:00:24 +0900

    Modify switch statement.

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -36,6 +36,7 @@ public:
         FOLLOW    =    23,  // follow attempts to follow another vehicle or ground station
         ZIGZAG    =    24,  // ZIGZAG mode is able to fly in a zigzag manner with predefined point A and point B
         SYSTEMID  =    25,  // System ID mode produces automated system identification signals in the controllers
+        NEW       =    26,
     };
 
     // constructor
